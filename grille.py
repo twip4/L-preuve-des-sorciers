18939a02bdec54ca3549b20207cc0b169cd07a7d
maxdan_ <35487233+minaoh06@users.noreply.github.com>
2024-01-09 11:25:56 +0100

    Ajout d'objet Case dans la grille dans l'éventualité d'un stockage d'information dans les cases

--- grille.py
+++ grille.py
@@ -1,16 +1,27 @@
 from random import randint
 
 
-def randint_zero_exclu(min, max):
+def randint_zero_exclu(val_min, val_max):
     while True:
-        num = randint(min, max)
+        num = randint(val_min, val_max)
         if num != 0:
             return num
 
 
+class Case:
+    def __init__(self, val_min, val_max):
+        # self.position = (0, 0)
+        self.valeur = randint_zero_exclu(val_min, val_max)
+
+    def get_valeur(self):
+        return self.valeur
+
+    def set_valeur(self, valeur):
+        self.valeur = valeur
+
+
 class Grille:
     """Classe Grille qui est une matrice qui représente une grille de nombre"""
-    matrice = None
 
     def __init__(self, taille, val_min=-5, val_max=5):
         self.taille = taille
@@ -20,19 +31,25 @@ class Grille:
 
     def affichage_matrice(self):
         for ligne in self.matrice:
-            ligne_formattee = ' '.join([
-           f'\033[92m{element: >3}\033[0m' if element > 0 else f'\033[91m{element: >3}\033[0m' if element < 0 else f'{element: >3}'for element in ligne])
-            print(ligne_formattee)
+            ligne_formate = ' '
+            for case in ligne:
+                if isinstance(case.get_valeur(), int) and case.get_valeur() > 0:
+                    ligne_formate += f'\033[92m{case.valeur: >3}\033[0m '
+                elif isinstance(case.get_valeur(), int) and case.get_valeur() < 0:
+                    ligne_formate += f'\033[91m{case.valeur: >3}\033[0m '
+                else:
+                    ligne_formate += f'{case.valeur: >3} '
+            print(ligne_formate)
 
     def generation_matrice(self):
-        return [[randint_zero_exclu(self.val_min, self.val_max, ) for _ in range(self.taille)] for _ in range(self.taille)]
+        return [[Case(self.val_min, self.val_max) for _ in range(self.taille)] for _ in range(self.taille)]
 
     def remplacement_valeur_etoile(self, x, y):
-        self.matrice[x][y] = 0
+        self.matrice[x][y].set_valeur('*')
 
 
 grille = Grille(20)
 grille.affichage_matrice()
-grille.remplacement_valeur_etoile(0,0)
+grille.remplacement_valeur_etoile(0, 0)
 print("\n")
 grille.affichage_matrice()
