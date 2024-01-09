77cd2359bd3a7f78141d5664b27b43adc1b11ccc
Twip4 <paulbaudinot@gmail.com>
2024-01-09 12:16:24 +0100

    pygame init

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 from sorcier import *
+from grille import *
+from main_menu_page import *
+import pygame
 
-if __name__ == '__main__':
-    sorcier = Sorcier(10)
-    sorcier.move("DOWN")
-    sorcier.move("RIGHT")
-    sorcier.move("DOWN")
-    sorcier.move("RIGHT")
-    sorcier.affiche()
+grille = Grille(40)
+matrice = grille.get_matrice()
+zone = [1280, 600]
+width_case = zone[0] // grille.get_size()
+height_case = zone[1] // grille.get_size()
+start_pos = [0, 60]
+
+pygame.init()
+
+width, height = 1280, 720
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+
+main_page = Main_menu_page(screen)
+
+main_page.run()
+
+screen.fill((0, 0, 0))
+
+for x in range(0, grille.get_size()):
+    for y in range(0, grille.get_size()):
+        start_pos[1] = (start_pos[1] + height_case) % zone[1]
+        if matrice[x][y].get_valeur() > 0:
+            pygame.draw.rect(screen, (40, 180, 99), (start_pos[0], start_pos[1], width_case, height_case))
+        else:
+            pygame.draw.rect(screen, (192, 57, 43), (start_pos[0], start_pos[1], width_case, height_case))
+    start_pos[0] = (start_pos[0] + width_case) % zone[0]
+pygame.display.flip()
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    pygame.time.Clock().tick(60)
--- sorcier.py
+++ sorcier.py
@@ -9,15 +9,14 @@ class Sorcier:
         elif direction == "RIGHT":
             self.position = self.position[0], self.position[1] + 1
         else:
-            print("error")
+            print("ERROR INPUT DIRECTION")
 
     def set_mana(self, mana):
         self.mana += mana
-        return self.mana
 
     def get_position(self):
         return self.position
 
-    def affiche(self):
+    def show(self):
         print("nb mana : " + str(self.mana))
         print("position : " + str(self.position))
