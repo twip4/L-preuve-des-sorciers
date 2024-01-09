2f077f8b2a29ec1cbb1b659cf085acf35522af0f
maxdan_ <35487233+minaoh06@users.noreply.github.com>
2024-01-09 14:05:06 +0100

    prototype de fonction de parcours

---
+++ parcours.py
@@ -0,0 +1,16 @@
+def chemin_mana_min():
+    """
+    Chemin-man-min
+    Cette fonction permet de trouver le chemin qui demande moins de mana au départ pour parcourir la grille
+    :return: liste des tuples des coordonnées des cases à parcourir
+    """
+    pass
+
+
+def chemin_optimal():
+    """
+    Chemin-optimal
+    Cette fonction permet de trouver le chemin minimal pour parcourir une grille avec un magicien
+    :return: liste des tuples des coordonnées des cases à parcourir
+    """
+    pass
