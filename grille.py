3da96e6128b868b05ec092727aeb81a09015b5ef
maxdan_ <35487233+minaoh06@users.noreply.github.com>
2024-01-09 12:33:40 +0100

    Modification de la grille pour pouvoir faire des grilles avec nombre de colonnes et lignes !=

--- grille.py
+++ grille.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 def randint_zero_exclu(val_min, val_max):
+    """Fonction randint avec exclusion de la valeur 0"""
     while True:
         num = randint(val_min, val_max)
         if num != 0:
@@ -10,27 +11,62 @@ def randint_zero_exclu(val_min, val_max):
 
 class Case:
     def __init__(self, val_min, val_max):
+        """
+
+        :param val_min: valeur minimum de la case
+        :param val_max: valeur maximum de la case
+        """
         # self.position = (0, 0)
         self.valeur = randint_zero_exclu(val_min, val_max)
 
     def get_valeur(self):
+        """Retourne la valeur de la case"""
         return self.valeur
 
     def set_valeur(self, valeur):
+        """Set la valeur de la case"""
         self.valeur = valeur
 
 
 class Grille:
     """Classe Grille qui est une matrice qui représente une grille de nombre"""
 
-    def __init__(self, taille, val_min=-5, val_max=5):
-        self.taille = taille
+    def __init__(self, x, y, val_min=-5, val_max=5):
+        """
+        :param x: nombre de lignes
+        :param y: nombre de colonnes
+        :param val_min: valeur minimum des cases
+        :param val_max: valeur maximum des cases
+        """
+        self.x = x
+        self.y = y
         self.val_min = val_min
         self.val_max = val_max
         self.matrice = self.generation_matrice()
-        self.remplacement_valeur_etoile(0, 0, '>')
+        self.remplacement_valeur_etoile((0, 0), '>')
+
+    def generation_matrice(self):
+        """Génère une matrice de x ligne et y colonne"""
+        return [[Case(self.val_min, self.val_max) for _ in range(self.y)] for _ in range(self.x)]
+
+    def remplacement_valeur_etoile(self, position, char='*'):
+        """Remplace une des valeurs de la matrice au coordonné x, y données avec le caractère char
+        :param position : tuple x, y de la position de la valeur à remplacer
+        :param char: caractère de remplacement
+        : return True si remplacement fait sinon False
+        """
+        if position[0] < 0 or position[1] < 0 or position[0] >= self.x or position[1] >= self.y:
+            return None
+        self.matrice[position[0]][position[1]].set_valeur(char)
+        return True
 
     def affichage_matrice(self):
+        """
+        Affiche la matrice en mettant en couleurs les caractère nécessaire :
+            rouge les valeurs < 0 des cases
+            vert les valeurs >0 des cases
+            blanc le reste.
+        """
         for ligne in self.matrice:
             ligne_formate = ' '
             for case in ligne:
@@ -42,12 +78,14 @@ class Grille:
                     ligne_formate += f'{case.valeur: >3} '
             print(ligne_formate)
 
-    def generation_matrice(self):
-        return [[Case(self.val_min, self.val_max) for _ in range(self.taille)] for _ in range(self.taille)]
+    def get_matrice(self):
+        """Retourne la matrice"""
+        return self.matrice
 
-    def remplacement_valeur_etoile(self, x, y, char='*'):
-        self.matrice[x][y].set_valeur(char)
+    def get_taille(self):
+        """Retourne la taille x, y de la matrice"""
+        return self.x, self.y
 
 
-grille = Grille(5)
+grille = Grille(5, 5)
 grille.affichage_matrice()
