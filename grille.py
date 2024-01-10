f082680512c9b8d4d0dda40acc0ba960fc9efe36
maxdan_ <35487233+minaoh06@users.noreply.github.com>
2024-01-10 12:30:17 +0100

    chemin_mana_min terminé

--- grille.py
+++ grille.py
@@ -10,13 +10,13 @@ def randint_zero_exclu(val_min, val_max):
 
 
 class Case:
-    def __init__(self, val_min, val_max):
+    def __init__(self, position, val_min, val_max):
         """
 
         :param val_min: valeur minimum de la case
         :param val_max: valeur maximum de la case
         """
-        # self.position = (0, 0)
+        self.position = position
         self.valeur = randint_zero_exclu(val_min, val_max)
 
     def get_valeur(self):
@@ -27,9 +27,13 @@ class Case:
         """Set la valeur de la case"""
         self.valeur = valeur
 
+    def get_position(self):
+        """Retourne la position de la case"""
+        return self.position
 
 class Grille:
     """Classe Grille qui est une matrice qui représente une grille de nombre"""
+    matrice = None
 
     def __init__(self, x, y, val_min=-5, val_max=5):
         """
@@ -38,16 +42,18 @@ class Grille:
         :param val_min: valeur minimum des cases
         :param val_max: valeur maximum des cases
         """
-        self.x = x
-        self.y = y
         self.val_min = val_min
         self.val_max = val_max
-        self.matrice = self.generation_matrice()
-        self.remplacement_valeur_etoile((0, 0), '>')
+        if x > 0 and y > 0:
+            self.x = x
+            self.y = y
+            self.generation_matrice()
+            self.remplacement_valeur_etoile((0, 0), '>')
+            self.remplacement_valeur_etoile((x-1, y-1), '>')
 
     def generation_matrice(self):
         """Génère une matrice de x ligne et y colonne"""
-        return [[Case(self.val_min, self.val_max) for _ in range(self.y)] for _ in range(self.x)]
+        self.matrice = [[Case((x, y), self.val_min, self.val_max) for y in range(self.y)] for x in range(self.x)]
 
     def remplacement_valeur_etoile(self, position, char='*'):
         """Remplace une des valeurs de la matrice au coordonné x, y données avec le caractère char
@@ -71,12 +77,40 @@ class Grille:
             ligne_formate = ' '
             for case in ligne:
                 if isinstance(case.get_valeur(), int) and case.get_valeur() > 0:
-                    ligne_formate += f'\033[92m{case.valeur: >3}\033[0m '
+                    ligne_formate += f'\033[92m{case.valeur: >5}\033[0m '
                 elif isinstance(case.get_valeur(), int) and case.get_valeur() < 0:
-                    ligne_formate += f'\033[91m{case.valeur: >3}\033[0m '
+                    ligne_formate += f'\033[91m{case.valeur: >5}\033[0m '
                 else:
-                    ligne_formate += f'{case.valeur: >3} '
+                    ligne_formate += f'{case.valeur: >5} '
             print(ligne_formate)
+
+    def copie_partielle_grille(self, position):
+        """
+        Copie partielle de la grille à partir d'un point
+
+        exemple :
+         > | 4 |-1
+        -4 |-1 | 2
+         3 | 2 |-1
+        -4 | 3 |-5
+
+        copie à partir de (1, 1) :
+         -1 | 2
+          2 |-1
+          3 |-5
+
+        copie à partir de (2, 1)
+        2 |-1
+        3 |-5
+
+        :return: Grille
+        """
+        retour = Grille(0, 0)
+        retour.x = self.x - position[0]
+        retour.y = self.y - position[1]
+        retour.matrice = [[element for j, element in enumerate(ligne) if j >= position[1]]
+                          for i, ligne in enumerate(self.matrice) if i >= position[0]]
+        return retour
 
     def get_matrice(self):
         """Retourne la matrice"""
@@ -85,3 +119,9 @@ class Grille:
     def get_taille(self):
         """Retourne la taille x, y de la matrice"""
         return self.x, self.y
+
+    def get_case(self, position):
+        """Retourne la case en fonction de la position"""
+        if (position[0] >= 0 and position[1] >= 0) and (position[0] < self.x and position[1] < self.y):
+            return self.matrice[position[0]][position[1]]
+        return False
--- parcours.py
+++ parcours.py
@@ -1,10 +1,52 @@
-def chemin_mana_min():
+from sorcier import *
+from grille import *
+from copy import copy
+from collections import deque
+from time import time
+
+
+def chemin_mana_min(grille: Grille, sorcier: Sorcier, mana_depense=0):
     """
-    Chemin-man-min
+    Chemin-mana-min
     Cette fonction permet de trouver le chemin qui demande moins de mana au départ pour parcourir la grille
     :return: liste des tuples des coordonnées des cases à parcourir
     """
-    pass
+    # Condition d'arrêt
+    if grille.x < 1 and grille.y < 1:
+        return mana_depense, deque()
+
+    # Variables
+    mana_droite, mana_bas = None, None
+    list_case_chemin_droite, list_case_chemin_bas = deque(), deque()
+    case_droite, case_bas = grille.get_case((0, 1)), grille.get_case((1, 0))
+
+    # Continuation du chemin
+    if case_droite and isinstance(case_droite.get_valeur(), int) and sorcier.mana + case_droite.get_valeur() > 0:
+        sorcier_droite = copy(sorcier)
+        sorcier_droite.mana += case_droite.get_valeur()
+        mana_droite, list_case_chemin_droite = chemin_mana_min(grille.copie_partielle_grille((0, 1)),
+                                                               sorcier_droite,
+                                                               mana_depense + case_droite.get_valeur())
+
+    if case_bas and isinstance(case_bas.get_valeur(), int) and sorcier.mana + case_bas.get_valeur() > 0:
+        sorcier_bas = copy(sorcier)
+        sorcier_bas.mana += case_bas.get_valeur()
+        mana_bas, list_case_chemin_bas = chemin_mana_min(grille.copie_partielle_grille((1, 0)),
+                                                         sorcier_bas,
+                                                         mana_depense + case_bas.get_valeur())
+
+    # Renvoie des valeurs avec test qi les valeurs sont None
+    if mana_droite is None and mana_bas is None:
+        return mana_depense, deque()
+
+    elif (mana_droite is not None and mana_bas is None) or (
+            mana_droite is not None and mana_bas is not None and mana_droite > mana_bas):
+        list_case_chemin_droite.append(case_droite.get_position())
+        return mana_depense + mana_droite, list_case_chemin_droite
+
+    else:  # if (mana_droite is None and mana_bas is not None) or (mana_bas < mana_droite):
+        list_case_chemin_bas.append(case_bas.get_position())
+        return mana_depense + mana_bas, list_case_chemin_bas
 
 
 def chemin_optimal():
@@ -14,3 +56,19 @@ def chemin_optimal():
     :return: liste des tuples des coordonnées des cases à parcourir
     """
     pass
+
+
+grille = Grille(5, 5)
+grille.affichage_matrice()
+sorcier = Sorcier(20)
+
+start = time()
+mana, list_case_chemin = chemin_mana_min(grille, sorcier)
+stop = time()
+print("time : ", stop - start)
+
+for i in list_case_chemin:
+    grille.remplacement_valeur_etoile(i)
+print(mana)
+print(list_case_chemin)
+grille.affichage_matrice()
