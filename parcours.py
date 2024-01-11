b3b5163c00643a6a102fddf03e89f82ba8e8ec8b
maxdan_ <35487233+minaoh06@users.noreply.github.com>
2024-01-11 09:42:16 +0100

    nouveau parcours chemin mana depart min

--- parcours.py
+++ parcours.py
@@ -1,119 +1,47 @@
-from sorcier import *
 from grille import *
-from copy import copy
-from collections import deque
-from time import time
-import heapq
 
 
-def chemin_mana_min(grille: Grille, sorcier: Sorcier):
-    def est_valide(x, y):
-        return 0 <= x < grille.x and 0 <= y < grille.y
-
-    def voisins(x, y):
-        for dx, dy in [(0, 1), (1, 0)]:  # Se déplacer à droite ou en bas
-            nx, ny = x + dx, y + dy
-            if est_valide(nx, ny):
-                yield nx, ny
-
-    start = (0, 0)
-    end = (grille.x - 1, grille.y - 1)
-    mana_initial = sorcier.mana
-    pq = [(-mana_initial, start, [])]  # File de priorité, le mana est négatif pour le min heap
-    visité = set()
-
-    while pq:
-        mana_neg, (x, y), chemin = heapq.heappop(pq)
-        mana = -mana_neg  # Convertir le mana en valeur positive pour le calcul
-
-        if (x, y) in visité:
-            continue
-        visité.add((x, y))
-
-        chemin_actuel = chemin + [(x, y)]
-
-        # Vérifier si la destination est atteinte
-        if (x, y) == end:
-            return mana, chemin_actuel
-
-        for nx, ny in voisins(x, y):
-            if (nx, ny) == start or (nx, ny) == end:
-                valeur_case = 0  # Pas de modification de mana pour les cases spéciales
-            else:
-                valeur_case = grille.get_case((nx, ny)).get_valeur()
-
-            nouveau_mana = mana + valeur_case
-
-            if nouveau_mana > 0:  # Le sorcier doit avoir un mana positif pour continuer
-                heapq.heappush(pq, (-nouveau_mana, (nx, ny), chemin_actuel))
-
-    return float('-inf'), []  # Aucun chemin trouvé
-
-
-def chemin_mana_min2(grille: Grille, sorcier: Sorcier, mana_depense=0):
-    """
-    Chemin-mana-min
-    Cette fonction permet de trouver le chemin qui demande moins de mana au départ pour parcourir la grille
-    :return: liste des tuples des coordonnées des cases à parcourir
-    """
-    # Condition d'arrêt
-    if grille.x < 1 and grille.y < 1:
-        return mana_depense, deque()
-
-    # Variables
-    mana_droite, mana_bas = None, None
-    list_case_chemin_droite, list_case_chemin_bas = deque(), deque()
-    case_droite, case_bas = grille.get_case((0, 1)), grille.get_case((1, 0))
-
-    # Continuation du chemin
-    if case_droite and isinstance(case_droite.get_valeur(), int) and sorcier.mana + case_droite.get_valeur() > 0:
-        sorcier_droite = copy(sorcier)
-        sorcier_droite.mana += case_droite.get_valeur()
-        mana_droite, list_case_chemin_droite = chemin_mana_min(grille.copie_partielle_grille((0, 1)),
-                                                               sorcier_droite,
-                                                               mana_depense + case_droite.get_valeur())
-
-    if case_bas and isinstance(case_bas.get_valeur(), int) and sorcier.mana + case_bas.get_valeur() > 0:
-        sorcier_bas = copy(sorcier)
-        sorcier_bas.mana += case_bas.get_valeur()
-        mana_bas, list_case_chemin_bas = chemin_mana_min(grille.copie_partielle_grille((1, 0)),
-                                                         sorcier_bas,
-                                                         mana_depense + case_bas.get_valeur())
-
-    # Renvoie des valeurs avec test qi les valeurs sont None
-    if mana_droite is None and mana_bas is None:
-        return mana_depense, deque()
-
-    elif (mana_droite is not None and mana_bas is None) or (
-            mana_droite is not None and mana_bas is not None and mana_droite > mana_bas):
-        list_case_chemin_droite.append(case_droite.get_position())
-        return mana_depense + mana_droite, list_case_chemin_droite
-
-    else:  # if (mana_droite is None and mana_bas is not None) or (mana_bas < mana_droite):
-        list_case_chemin_bas.append(case_bas.get_position())
-        return mana_depense + mana_bas, list_case_chemin_bas
-
-
-def chemin_optimal():
+def chemin_mana_depart_min(grille: Grille):
     """
     Chemin-optimal
     Cette fonction permet de trouver le chemin minimal pour parcourir une grille avec un magicien
     :return: liste des tuples des coordonnées des cases à parcourir
     """
-    pass
+    x, y = grille.get_taille()
+    cout = [[0 for _ in range(y)] for _ in range(x)]
 
+    # Initialisation de la position de départ
+    cout[0][0] = grille.get_case((0, 0)).get_valeur()
 
-grille = Grille(50, 50)
-grille.affichage_matrice()
-sorcier = Sorcier(20)
+    # Calcul des coûts
+    # Première ligne
+    for j in range(1, y):
+        cout[0][j] = cout[0][j - 1] + grille.get_case((0, j)).get_valeur()
 
-start = time()
-mana, list_case_chemin = chemin_mana_min(grille, sorcier)
-stop = time()
-print("time : ", stop - start)
+    # Première colonne
+    for i in range(1, x):
+        cout[i][0] = cout[i - 1][0] + grille.get_case((i, 0)).get_valeur()
 
-for i in list_case_chemin:
-    grille.remplacement_valeur_etoile(i)
-print(mana)
-print(list_case_chemin)
-grille.affichage_matrice()
+    # Autres cases
+    for i in range(1, x):
+        for j in range(1, y):
+            cout[i][j] = max(cout[i - 1][j], cout[i][j - 1]) + grille.get_case((i, j)).get_valeur()
+
+    # Retracer le chemin
+    chemin = []
+    i, j = x - 1, y - 1
+    while i >= 0 and j >= 0:
+        chemin.append((i, j))
+        if i == 0 and j == 0:
+            break
+        elif i == 0:
+            j -= 1
+        elif j == 0:
+            i -= 1
+        else:
+            if cout[i - 1][j] > cout[i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+
+    return chemin[::-1]
